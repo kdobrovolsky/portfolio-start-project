9de55ef881181b9822f370ceb04fb80347c6596f
Добровольский <kdobrovolsky02@gmail.com>
2025-12-15 15:55:16 +0300

    deploy project, refactor package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ["babel-plugin-styled-components"],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  }
-});
+  plugins: [react()],
+  base: '/portfolio-start-project/'
+})
